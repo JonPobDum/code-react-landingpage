11dae0a87610f8431bd27c6a1056cd1e147e83e8
JonPobDum <jpoblete86@gmail.com>
2022-12-21 10:21:25 +0000

    0002

---
+++ src/js/component/footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Footer () {
+return (
+<footer class="text-muted py-2 p-3 mb-2 bg-dark text-white">
+  <div class="container">
+    <p class="mb-1 text-white text-center">Copyright © your Website 2018</p>
+
+  </div>
+</footer>
+
+)
+}
+
+export default Footer;
